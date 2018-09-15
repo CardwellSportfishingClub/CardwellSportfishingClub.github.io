659a28c508a769ce57d0182d3dfe4813f3ef509e
Garrath Leeds <LeedsG@sc-au.com.au>
2018-09-15 12:29:50 +1000

    fixed compatibility issues, updated navigation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -42,7 +42,32 @@ $("#ContactButton").click(function () {
 });
 
 
-$("#ContactFrame").load(function () {
+$("#ContactFrame").on('load', function () {
     $content = $("#ContactSection");
     $content.slideToggle(500)
-})
+});
+
+
+
+$(document).ready(function(){
+	$('.customer-logos').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		arrows: false,
+		dots: false,
+			pauseOnHover: true,
+			responsive: [{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 3
+			}
+		}, {
+			breakpoint: 520,
+			settings: {
+				slidesToShow: 2
+			}
+		}]
+	});
+});
